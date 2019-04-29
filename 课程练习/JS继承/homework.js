be38e2cca29c29349abaacf74a8c4b06240fe8b7
Huangfuyuru <https://github.com/Huangfuyuru/JS-advanced-lessons.git>
2019-04-29 09:44:09 +0800

    a

--- 课程练习/JS继承/homework.js
+++ 课程练习/JS继承/homework.js
@@ -1,79 +1,204 @@
-//关系一
+/*写在前面 
+一、普通对象与函数对象
+    在JS中，万物皆对象，但是也是有区别的，分为普通对象和函数对象,
+    (我们也可以把函数对象也可以理解为构造函数),Object、Function是JS自带的函数对象,
+    如何来分辨函数对象和普通对象？
+    通过new Function()创建的对象都是函数对象，其他是普通对象，
+    那么Object\Function因为也是通过new Function创建的，所以也是函数对象
+    
+二、原型对象
+    在JS中，每当定义一个普通对象(函数对象)时候，对象中都会包含一些预定义的属性。
+    其中函数对象的一个属性就是原型对象prototype,普通对象的原型对象就是__proto__
+
+    而原型对象[.prototype 或  .__proto__]的实质是普通对象[普通对象有__proto__ constructor],
+    (Function.prototype除外，他是函数对象，虽然他是函数对象，
+    但他很特殊他没有prototype属性[函数对象都有prototype属性]，
+    像Foo.prototype是普通对象)
+三、__proto__
+    f1.__proto__=Foo.prototype
+    第一种理解：f1的原型对象，他继承自Foo.prototype
+    第二种理解：指向创建他(f1)的函数对象(Foo)的原型对象prototype 
+四、
+    __proto__ constructor是普通对象独有的，
+    prototype是函数对象独有的，但是函数对象作为普通对象的时候
+    拥有__proto__ constructor
+五、
+    原型链的尽头是Object.prototype 所有对象均从Object.prototype继承属性
+    也就是说Object.prototype没有原型对象,Object.prototype.__proto__不存在
+    他不能继承自任何对象
+*/
+
+//关系一: f1.__proto__==Foo.prototype
 function Foo(userName,age){
     this.userName=userName;
     this.age=age;
 };
-Foo.prototype.sayHello=function(){
-    return this.userName+" say hello";
+Foo.prototype.sayHello1=function(){
+    return this.userName+" say hello1";
 }
 var f1=new Foo("zhangsan",20);
-/*
-    关系一:f1对象的__proto__属性与其构造函数Foo的prototype属性指向相同，
-    所以通过Foo实例化得到的对象可以去Foo.prototype上查找方法，例如sayHello方法
-
-*/
 console.log(f1.__proto__==Foo.prototype);
-console.log(f1.sayHello());
+console.log(f1.sayHello1());
+/*
+    f1.__proto__指向创建他(f1)的函数对象(Foo)的原型对象prototype,
+    (f1继承自Foo.prototype)
+    因为sayHello是定义在Foo.prototype上的，所以f1可以继承得到sayHello1函数
 
-//关系二
+*/
+
+//关系二: Foo.prototype.__proto__==Object.prototype
 function Foo(userName,age){
     this.userName=userName;
     this.age=age;
 };
-Object.prototype.sayHello=function(){
-    return this.userName+" say hello";
+Object.prototype.sayHello2=function(){
+    return this.userName+" say hello2";
 };
 var f1=new Foo("zhangsan",20);
-var f2={userName:"lisi",age:20};
-/*
-    关系二:f1对象的原型对象(父)的原型对象(爷)和函数Object的原型对象相同，
-    因为f1.__proto__=Foo.prototype,所以也可以理解为函数Foo的原型对象(父)
-    的原型对象(爷)与Object的原型对象相同
-    f1.__proto__.__proto__==Object.prototype
-    Foo.prototype.__proto__==Object.prototype
-
-*/
-console.log(f1.__proto__.__proto__==Object.prototype);
 console.log(Foo.prototype.__proto__==Object.prototype);
-/*
-    为什么会相等呢？
-    我们可以这样理解
-    我们知道任何继承自Object,也可以理解为是用Object函数来创建,
-    如果一个对象不是用构造函数创建，而是使用字面量的方式创建，比如f2,
-    那么f2.__proto__=Object.prototype,也就是说f2的父对象就是Object的函数对象
+console.log(Foo.prototype.sayHello2());
+console.log(f1.sayHello2());
 
-    而f1是通过Foo这个构造函数创建的，所以f1.__proto__==Foo.prototype,
-    f1.__proto__(Foo.prototype)相当于'f2',所以'f2'的父对象是Object的函数对象
-
-*/
-console.log(f2.__proto__==Object.prototype);
 /*
-    我们来验证一下
-    添加在Object.prototype上的sayHello函数f1,f2都可以调用到
-*/
-console.log(f1.sayHello());
-console.log(f2.sayHello());
-
-//关系三
-var f2={userName:"lisi",age:20};
-console.log(f2.__proto__);
-console.log(f2.__proto__.__proto__);
-console.log(Object.prototype.__proto__);
-/*
-    关系三:Object.prototype的原型对象是null,因为Object.prototype没有原型
+    f1继承自Foo.prototype,Foo.prototype是普通对象，
+    他的__proto__指向创建他(Foo.prototype他是个对象)
+    的函数对象(Object)的prototype
+    (Foo.prototype继承自Object.prototype)
+    所以当我们在Object.prototype上定义sayHello2函数时，
+    Foo.prototype可以调用sayHello,f1也可以调用sayHello2()
 */
 
-//关系四
+//关系三: Object.prototype.__proto__==null
+console.log(Object.prototype.__proto__==null)
+/*
+    Object.prototype(这是个普通对象),他的__proto__指向创建他(Object.prototype)
+    的构造函数的prototype,但是并没有一个构造函数是创建了他，所以是null
+    也可以把Object.prototype理解为是所有原型的祖宗，到他这里为止，在也没有父代
+*/
 
-//关系五
+//关系四: Foo.prototype==Foo.prototype
+console.log(Foo.prototype==Foo.prototype);
+/*
+    每个函数对象生成时自动生成一个prototype
+*/
+
+//关系五: Foo.prototype.constructor==Foo
 function Foo(userName,age){
     this.userName=userName;
     this.age=age;
 };
-Foo.prototype.sayHello=function(){
-    return this.userName+" say hello";
-}
 var f1=new Foo("zhangsan",20);
+console.log(Foo.prototype.constructor==Foo);
+console.log(f1.__proto__.constructor==Foo);
 /*
-    先理解一下const
+
+    函数对象的原型对象prototype中的constructor属性，
+    指向他的函数对象(构造函数)
+    普通对象的原型对象__proto__中的constructor属性，
+    指向引用他的函数对象(构造函数)
+   
 */
+
+//关系六： Foo.__proto__==Function.prototype
+function Foo(userName,age){
+    this.userName=userName;
+    this.age=age;
+};
+Function.prototype.sayHello3=function(){
+    return "hello3";
+};
+console.log(Foo.__proto__==Function.prototype);
+console.log(Foo.sayHello3());
+/*
+    Foo是一个函数对象，他的原型对象是Foo.prototype,
+    但同时也可以理解为一个普通对象，Foo.__proto__指向创建他(Foo)的构造
+    函数(Function)的原型对象prototype
+    (Foo继承自Function.prototype),所以在Function.prototype上定义的sayHello3
+    函数，在Foo上也可以调用
+*/
+
+//关系七：Function.prototype.__proto__==Object.prototype
+Object.prototype.sayHello4=function(){
+    return "hello4";
+}
+console.log(Function.prototype.__proto__==Object.prototype);
+console.log(Function.prototype.sayHello4());
+console.log(Array instanceof Function);
+console.log(Function instanceof Object);
+/*
+    Function.prototype直接继承Object.prototype,
+    所以Function.prototype.__proto__==Object.prototype,
+    当在Object.prototype上定义sayHello4函数时，Function.prototype
+    也可以调用.
+
+    Function|Object|Array.__proto__继承自Function.prototype
+    Function.prototype.__proto__继承自Object.prototype
+*/
+
+//关系八: o1.__proto__==Object.prototype
+var o1=new Object();
+o1.name="zhangsan";
+Object.prototype.sayHello5=function(){
+    return this.name+" say hello5";
+}
+console.log(o1.__proto__==Object.prototype);
+console.log(o1.sayHello5());
+console.log(Object.prototype);
+/*
+    o1.__proto__指向创建o1的构造函数(Object)的原型对象prototype
+    也可以理解为o1继承Object.prototype
+    当在Object.prototype上定义sayHello5这个函数的时候，o1也可以调用到
+*/
+
+///关系九:Object.prototype==Object.prototype
+console.log(Object.prototype==Object.prototype);
+/*
+    一个构造函数(函数对象)生成时，自动生成一个prototype属性，指向他的原型对象
+*/
+
+//关系十:Object.prototype.constructor==Object
+console.log(Object.prototype.constructor==Object);
+/*
+    函数对象的原型对象prototype中的constructor属性，
+    指向他的函数对象(构造函数)
+    普通对象的原型对象__proto__中的constructor属性，
+    指向引用他的函数对象(构造函数)
+*/
+
+//关系十一：Object.__proto__==Function.prototype
+Function.prototype.sayHello6=function(){
+    return "hello6";
+};
+console.log(Object.sayHello6());
+console.log(Object.__proto__==Function.prototype);
+/*
+    Object是函数对象，是通过new Function()创建的，
+    但是我们也可以理解为他是一个普通对象，
+    Object.__proto__含义是指向创建他(Object)的构造函数(Function)
+    的原型对象prototype(Object继承自Function.prototype)
+    所以在Function.prototype上定义的sayHello6()函数,Object可以调用
+*/
+
+//关系十二:Function.prototype==Function.prototype
+console.log(Function.prototype==Function.prototype);
+/*
+    每个函数对象生成时自动生成一个原型对象prototype
+*/
+
+//关系十三: Function.prototype.constructor==Function
+console.log(Function.prototype.constructor==Function);
+/*
+    函数对象的原型对象prototype中的constructor属性，
+    指向他的函数对象(构造函数)
+    普通对象的原型对象__proto__中的constructor属性，
+    指向引用他的函数对象(构造函数)
+*/
+
+//关系十四
+console.log(Function.__proto__==Function.prototype);
+/*
+    关系十四：Function是函数对象，是通过new Function()创建的，
+    但是我们也可以理解为他是一个普通对象，
+    Function.__proto__含义是指向创建他(Function)的构造函数(Function)
+*/
+
